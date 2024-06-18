8fbc8172257b72fa2d069720141366aeac194db5
AlmogApiiro <almog@apiiro.com>
2024-06-18 18:14:51 +0300

    temp commit

--- UniSign/app/(tabs)/index.tsx
+++ UniSign/app/(tabs)/index.tsx
@@ -1,34 +1,144 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import axios from 'axios';
+const API_URL = 'http://192.168.0.102:3000';
 
 export default function TabOneScreen() {
+  const [roomId, setRoomId] = useState('');
+  useEffect(() => {
+    console.log(`roomId: ${roomId}`);
+  }, [roomId]);
+  const startMeeting = async () => {
+    const url = `${API_URL}/db/rooms/${roomId}`
+    if (!roomId) {
+      generateRandomId();
+    }
+  };
+
+  const generateRandomId = () => {
+    const characters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let result = '';
+    for (let i = 0; i < 16; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+    setRoomId(result);
+  };
+
+  const joinMeeting = async () => {
+    if (roomId) {
+      try {
+        const url = `${API_URL}/db/rooms/${roomId}`;
+        await axios
+          .get(url)
+          .then((response) => {
+            // joinCall()
+          })
+          .catch((error) => {
+            if (error.response) {
+              Alert.alert(
+                `Error ${error.response.status}: ${error.response.data}`
+              );
+              console.warn(
+                `Error ${error.response.status}: ${error.response.data}`,
+                `\nError headers: ${error.response.headers}`
+              );
+            } else if (error.request) {
+              console.warn('Error request:', error.request);
+            } else {
+              console.warn('Error message:', error.message);
+            }
+            // Uncomment for complex errors // console.log('Error config:', error.config);
+          });
+      } catch (error) {
+        console.error('Unexpected error:', error);
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{ flex: 1, backgroundColor: 'grey' }}>
+      <View style={styles.topAndBottom}>
+        <Text style={{ color: 'white', fontSize: 20 }}>Room Screen</Text>
+      </View>
+      <View style={{ flex: 8, backgroundColor: 'green', flexDirection: 'row' }}>
+        <View style={{ flex: 1, backgroundColor: 'grey' }}></View>
+        <View style={styles.middle}>
+          <View>
+            <Text>Enter Room ID:</Text>
+            <TextInput
+              style={styles.textBox}
+              value={roomId}
+              onChangeText={setRoomId}
+            />
+            <View>
+              <TouchableOpacity onPress={generateRandomId}>
+                <Text style={{ fontSize: 25 }}>Start meeting</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={joinMeeting}>
+                <Text style={{ fontSize: 25 }}>Join meeting</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'grey' }}></View>
+      </View>
+      <View style={styles.topAndBottom}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  topAndBottom: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold'
+  middle: {
+    flex: 8,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%'
+  textBox: {
+    borderWidth: 1,
+    borderColor: 'black'
   }
 });
+
+// axios request
+// const axiosHandler = async () => {
+//     try {
+//       const url = `${API_URL}/db/rooms/123`;
+//       console.log(url);
+//       await axios.get(url)
+//         .then((response) => {
+//           // succesful request
+//           console.log('Response data:', response.data);
+//           console.log('Response status:', response.status);
+//           console.log('Response statusText:', response.statusText);
+//           console.log('Response headers:', response.headers);
+//           console.log('Response config:', response.config);
+//         })
+//         .catch((error) => {
+//           if (error.response) {
+//             // The request was made and the server responded with a status code that falls out of the range of 2xx
+//             console.log('Error data:', error.response.data);
+//             console.log('Error status:', error.response.status);
+//             console.log('Error headers:', error.response.headers);
+//           } else if (error.request) {
+//             // The request was made but no response was received
+//             console.log('Error request:', error.request);
+//           } else {
+//             // Something happened in setting up the request that triggered an Error
+//             console.log('Error message:', error.message);
+//           }
+//           console.log('Error config:', error.config);
+//         });
+//     } catch (error) {
+//       console.error('Unexpected error:', error);
+//     }
+// };
--- server/index.js
+++ server/index.js
@@ -4,51 +4,69 @@ const PORT = 3000;
 
 const { sequelize } = require('./models/db');
 const Users = require('./models/users');
+const Rooms = require('./models/rooms');
 const bcrypt = require('bcrypt');
+const os = require('os');
 
+// const cors = require('cors');
+// app.use(cors());
 app.use(express.json());
 
+const getLocalIpAddress = () => {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    for (const iface of interfaces[interfaceName]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // Default to localhost if no external IP found
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 // Mock Video Call Service
 app.post('/api/video-call', (req, res) => {
-  res.json({ message: "Video call initiated" });
+  res.json({ message: 'Video call initiated' });
 });
 
 // Mock Audio Transcription Service
 app.post('/api/audio-transcription', (req, res) => {
-  res.json({ transcription: "This is a mock audio transcription" });
+  res.json({ transcription: 'This is a mock audio transcription' });
 });
 
 // Mock Video Transcription Service
 app.post('/api/video-transcription', (req, res) => {
-  res.json({ transcription: "This is a mock video transcription" });
+  res.json({ transcription: 'This is a mock video transcription' });
 });
 
-
+// Users
 app.get('/db/users', async (req, res) => {
   const records = await Users.findAll();
   res.json(records);
 });
 
-app.post('/sign-up', async (req, res) => {
+app.post('/db/users/sign-up', async (req, res) => {
   try {
-    const newRecord = await Users.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      usesService: req.body.usesService,
-      password: req.body.password
-    });
-    res.status(201).json(newRecord);
+    if (userExists()){
+      res.status(400).json('');
+    }
+    if (passwordsOk()) {
+      
+    } else {
+      console.error('wrong password');
+    }
+    console.log(`User ${userEmail} created`);
   } catch (error) {
     res.status(400).json({ error: error.message });
+    console.log(`Error in user creation: ${error.message}`);
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/db/users/login', async (req, res) => {
   const user = await Users.findOne({ where: { email: req.body.email } });
   if (!user) {
     return res.status(401).json({ error: 'User not found' });
@@ -63,14 +81,48 @@ app.post('/login', async (req, res) => {
   res.json({ message: 'Login successful' });
 });
 
+// Rooms
+app.get('/db/rooms', async (req, res) => {
+  console.log('here');
+  const records = await Rooms.findAll();
+  return res.json(records);
+});
+
+app.get('/db/rooms/:roomId', async (req, res) => {
+  const roomId = req.params.roomId;
+  const room = await Rooms.findOne({ where: { roomId: roomId } });
+  if (room) {
+    console.log(`Room ${room.roomId} exists`);
+    return res.status(200).json({ message: `Room ${room.roomId} exists` });
+  }
+  console.log(`Room ${roomId} does not exist`);
+  return res.sendStatus(404);
+  // return res.status(404).json({message: 'Not Found'})
+});
+
+app.post('/db/rooms/:roomId', async (req, res) => {
+  try {
+    const roomId = req.params.roomId;
+    const newRecord = await Rooms.create({
+      roomId: roomId,
+    });
+    res.status(201).json(newRecord);
+    console.log(`Room ${roomId} created`);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.log(`Error in room creation: ${error.message}`);
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://${getLocalIpAddress()}:${PORT}/`);
 
-  sequelize.sync().then(() => {
-    console.log('Database synchronized');
-  }).catch((error) => {
-    console.error('Error synchronizing database:', error);
-  });
-
+  sequelize
+    .sync()
+    .then(() => {
+      console.log('Database synchronized');
+    })
+    .catch((error) => {
+      console.error('Error synchronizing database:', error);
+    });
 });
